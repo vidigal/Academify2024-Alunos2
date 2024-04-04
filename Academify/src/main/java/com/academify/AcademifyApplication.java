32062ae6cdf212ce4749122d3533a22387c13ca3
Victor Vidigal Ribeiro <victor.ribeiro1@granbery.edu.dti>
2024-04-04 19:41:02 -0300

    criando projeto

---
+++ Academify/src/main/java/com/academify/AcademifyApplication.java
@@ -0,0 +1,13 @@
+package com.academify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademifyApplication.class, args);
+	}
+
+}
---
+++ Academify/src/test/java/com/academify/AcademifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
